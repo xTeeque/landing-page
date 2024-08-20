7a5ef6fcd7a5248e53d6c561c2eb917ee1415537
Asaf Arusi <84575853+xTeeque@users.noreply.github.com>
2024-08-20 23:08:51 +0300

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-/* Created by Tivotal */
+/* crads blurring button script */
+
+document.querySelectorAll('.toggle-button').forEach(button => {
+    button.addEventListener('click', () => {
+        button.closest('.card').classList.toggle('blurred');
+    });
+});
+
+/* testimonation swiping script */
 
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
@@ -14,3 +22,27 @@ var swiper = new Swiper(".mySwiper", {
   },
   loop: true,
 });
+
+/* form script */
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+			event.preventDefault(); // Prevent the default form submission
+			const form = event.target;
+			
+			fetch(form.action, {
+				method: 'POST',
+				body: new FormData(form),
+				headers: {
+					'Accept': 'application/json'
+				}
+			}).then(response => {
+				if (response.ok) {
+					alert('תודה שפנית אלינו!'); // Popup message
+					form.reset(); // Clear the form
+				} else {
+					alert('אירעה שגיאה, נסה שוב מאוחר יותר.');
+				}
+			}).catch(error => {
+				alert('אירעה שגיאה, נסה שוב מאוחר יותר.');
+			});
+		});
